172fc38a1974b7644a6579799a8ab36321bc5cfb
Superbil <superbil@gmail.com>
2016-09-06 21:12:10 +0800

    Fix wrong name and path for cask

--- Casks/run-emacs-with-debugger.rb
+++ Casks/run-emacs-with-debugger.rb
@@ -3,9 +3,9 @@ cask 'run-emacs-with-debugger' do
   sha256 '974fc83f9b654a3fdf8d88c9a4b1ca2339fe4b5326da5992b6969af50895764f'
 
   url "https://github.com/Superbil/Run-Emacs-app-With-Debugger/releases/download/#{version}/Run.Emacs.With.Debugger.zip"
-  name 'Run-Emacs-With-Debugger'
+  name 'Run Emacs With Debugger'
   homepage 'https://github.com/Superbil/Run-Emacs-app-With-Debugger'
   license :mit
 
-  app 'Run Emacs With Debugger'
+  app 'Run Emacs With Debugger.app'
 end
